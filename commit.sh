f9ec8707b0b701f201e3f3ed45c6e69b0f290353
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2025-12-19 15:16:05 +0700

    change: commit

--- commit.sh
+++ commit.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
 git add .
-git commit -m "$@"
+
+if [ "$1" = "add" ]; then
+    shift
+    git commit -m "add: $*"
+
+elif [ "$1" = "change" ]; then
+    shift
+    git commit -m "change: $*"
+
+elif [ "$1" = "remove" ]; then
+    shift
+    git commit -m "remove: $*"
+
+else
+    echo "Usage: $0 {add|change|remove} commit message"
+    exit 1
+fi
+
 git push
