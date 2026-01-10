2bec2e51b958900a0092b3757fe240a255e2d266
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-01-11 01:59:07 +0700

    change: foodlist

--- project1/script/components/loadfoodlist.js
+++ project1/script/components/loadfoodlist.js
@@ -8,12 +8,25 @@ fetch('data/menulist.json').then(res => res.text()).then(data =>
                 let menuelm = document.createElement('a');
 
                 menuelm.id = "menu";
-                menuelm.className = "flex flex-col items-center w-[250px] border p-5 rounded-xl cursor-pointer";
+                menuelm.className = "flex flex-col items-center w-[250px] border p-5 rounded-xl cursor-pointer opacity-0";
                 menuelm.innerHTML = comp;
                 menuelm.querySelector('img').src = menu[i].imglinks;
                 menuelm.querySelector('h1').textContent = menu[i].name;
 
                 document.getElementById('menulist').appendChild(menuelm);
+
+                menuelm.animate(
+                    {
+                        opacity: [0,1],
+                        transform: ['translateY(20px)', 'translateY(0px)']
+                    },
+                    {
+                        fill: "forwards",
+                        duration: 500,
+                        delay: i * 100,
+                        easing: 'ease-out'
+                    }
+                );
             });
         }
     }
---
+++ project1/script/components/loadmenu.js
@@ -0,0 +1,3 @@
+fetch('components/menu.html').then(res => res.text()).then(data => {
+    document.getElementById('menu').innerHTML = data;
+});
