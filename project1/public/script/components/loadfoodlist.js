fc21401e750cba9c931bbdfe3f11e7c09e88df73
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-01-12 20:29:46 +0700

    add: nodejs for tailwinf cli

--- project1/public/script/components/loadfoodlist.js
+++ project1/public/script/components/loadfoodlist.js
@@ -11,7 +11,7 @@ async function loadFoodTemplate() {
 function buildMenuItem(menu, template, index) {
     const menuelm = document.createElement('a');
 
-    menuelm.className = "flex flex-row lg:flex-col md:flex-col sm:flex-row items-center border-b-4 border-r-4 border-t border-l rounded-tl-3xl rounded-br-lg border-amber-500 bg-red p-5 cursor-pointer opacity-0 w-[500px] lg:w-[250px] md:w-[250px] sm:w-[500px]";
+    menuelm.className = `flex flex-row lg:flex-col md:flex-col sm:flex-row items-center border-b-8 border-r-4 border-t border-l rounded-tl-3xl rounded-br-lg border-red-800 p-5 cursor-pointer opacity-0 w-[500px] lg:w-[250px] md:w-[250px] sm:w-[500px] bg-ci-main-1 text-black text-xl text-center`;
     menuelm.href = `${menu.id}`
     menuelm.innerHTML = template;
 
---
+++ project1/public/script/utils/tailwindconfig.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "ci-main-1": "#F0D2AE",
+        "ci-main-2": "#E0B48C",
+      },
+    }
+  },
+  plugins: []
+}
